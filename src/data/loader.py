0bcc640e6d2143ab01c9f0fd1c1b7a6f9a3892ce
Ran Wright <ranman37@gmail.com>
2024-12-22 13:48:32 +0200

    loader

--- src/data/loader.py
+++ src/data/loader.py
@@ -1,31 +1,22 @@
 # Data loading and augmentation logic
-from datasets import load_dataset
-import matplotlib.pyplot as plt
-
-# Load the dataset
-pcam_source = "dpdl-benchmark/patch_camelyon"
 import logging
+from pydantic import BaseModel
+from datasets import load_dataset
 
 logging.basicConfig(level=logging.DEBUG)
-dataset = load_dataset(pcam_source, split="validation")
 
-# Extract the first image and label
-first_image = dataset[0]["image"]
-first_label = dataset[0]["label"]
 
-# Display the first image
-plt.imshow(first_image)
-plt.title(f"Label: {first_label}")
-plt.axis("off")
-plt.show()
+class Split(BaseModel):
+    Train = "train"
+    Test: str = "test"
+    Validation: str = "validation"
 
-# Calculate label distribution
-labels = [sample["label"] for sample in dataset]
-unique_labels, counts = zip(*[(label, labels.count(label)) for label in set(labels)])
 
-# Plot label distribution
-plt.bar(unique_labels, counts)
-plt.title("Label Distribution in Train Dataset")
-plt.xlabel("Labels")
-plt.ylabel("Counts")
-plt.show()
+def load_data(pcam_source, split):
+    dataset = load_dataset(pcam_source, split=split)
+
+
+if __name__ == "__main__":
+    pcam_source = "dpdl-benchmark/patch_camelyon"
+    split = Split.Validation
+    load_data(pcam_source, split)
